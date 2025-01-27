f885c5b64eb2768a365c2b91dc5eb768feeb61c1
boel78 <95147718+boel78@users.noreply.github.com>
2025-01-27 09:31:19 -0700

    pomodoro skapar sessionobjekt

--- PomodoroFrontend/pomodoro/app/pomodoro-session/page.tsx
+++ PomodoroFrontend/pomodoro/app/pomodoro-session/page.tsx
@@ -1,178 +1,117 @@
 "use client";
 import InputWithLabel from "@/components/ui/input-with-label";
 import { Button } from "@/components/ui/button";
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect, useRef} from "react";
 
 export default function Pomodoro() {
-  const Ref = useRef(null);
-  const isPomodoroRef = useRef(true);
+  const totalTimeRef = useRef(0);
+  const [isPomodoro, setIsPomodoro] = useState(true);
   const [timerIsActive, setTimerIsActive] = useState(false);
   const [timer, setTimer] = useState(0);
   const [pomodoroLength, setPomodoroLength] = useState(0);
   const [breakLength, setBreakLength] = useState(0);
-  const [totalTimeSeconds, setTotalTimeSeconds] = useState(0);
+  const [workedTime, setWorkedTime] = useState(0)
+  const [sessionType, setSessionType] = useState("")
   const preferedTime = "00:00:10";
   const preferedBreak = "00:00:05";
+  const algoritmSetting = "longerBreak";
 
-  /*useEffect(() => {
-    console.log("isPomodoro updated to: ", isPomodoro);
-  }, [isPomodoro]);*/
-
-  /*const getTimeRemaining = (e) => {
-    const total = Date.parse(e) - Date.parse(new Date());
-    const seconds = Math.floor((total / 1000) % 60);
-    const minutes = Math.floor((total / 1000 / 60) % 60);
-    const hours = Math.floor((total / 1000 / 60 / 60) % 24);
-    return {
-      total,
-      hours,
-      minutes,
-      seconds,
-    };
-  };
-  const startTimer = (e) => {
-    if (totalTimeSeconds > 0) {
-      let { total, hours, minutes, seconds } = getTimeRemaining(e);
-      if (total >= 0) {
-        setTimer(
-          (hours > 9 ? hours : "0" + hours) +
-            ":" +
-            (minutes > 9 ? minutes : "0" + minutes) +
-            ":" +
-            (seconds > 9 ? seconds : "0" + seconds)
-        );
-      }
-      //Timern tar slut
-      else {
-        console.log("Total time: " + totalTimeSeconds);
-
-        const isPomodoro = isPomodoroRef.current;
-        isPomodoroRef.current = !isPomodoro;
-        
-                
-        if (isPomodoro) {
-          console.log("AAAAAAA");
-          
-          
-          onClickReset(pomodoroLength);
-        } else {
-          console.log("BBBBBBB");
-          
-          onClickReset(breakLength);
-          
-        }
-        clearInterval(Ref.current)
-        setTimerIsActive(false);
-        setTimeout(() => {
-          setTimerIsActive(true);
-          onClickReset(isPomodoro ? pomodoroLength : breakLength);
-        }, 1000);
-      }
-    } else {
-      setTimerIsActive(false);
-    }
-  };
-
+  useEffect(() => {
+    if (!timerIsActive) return;
   
-
-  const clearTimer = (e) => {
-  if (Ref.current) clearInterval(Ref.current); // Clear previous interval
-  const id = setInterval(() => {
-    startTimer(e);
-  }, 1000); // Create new interval
-  Ref.current = id;
-};
-
-  const getDeadTime = (timeToSet) => {
-    let deadline = new Date();
-    deadline.setSeconds(deadline.getSeconds() + timeToSet);
-    return deadline;
-  };
-
-  const onClickReset = (timeToSet) => {
-    clearTimer(getDeadTime(timeToSet));
-  };*/
-
-  useEffect(() => {
-    if(timerIsActive){
-    const [preferedhours, preferedminutes, preferedseconds] = preferedTime
-      .split(":")
-      .map(Number);
-    const [preferedBreakhours, preferedBreakminutes, preferedBreakseconds] =
-      preferedBreak.split(":").map(Number);
-    
-    
-    const preferedTimeSeconds =
-      preferedhours * 3600 + preferedminutes * 60 + preferedseconds;
-    
-      const preferedBreakSeconds =
-      preferedBreakhours * 3600 +
-      preferedBreakminutes * 60 +
-      preferedBreakseconds;
-
-    const cycle_length = preferedBreakSeconds + preferedTimeSeconds;    
-
-    
-    const amount_of_cycles = Math.floor(totalTimeSeconds / cycle_length);
-
-    
-
-    const rest_time = totalTimeSeconds - amount_of_cycles * cycle_length;
-
-    const time_per_setting = rest_time / amount_of_cycles;
-
-    setPomodoroLength(preferedTimeSeconds);
-    setBreakLength(preferedBreakSeconds + time_per_setting);
-    setTimer(preferedTimeSeconds)
-    }
-  },[totalTimeSeconds])
-
-  useEffect(() => {
-    if (!timerIsActive) return; // If the timer isn't running, do nothing
-
     const interval = setInterval(() => {
       setTimer((prevTime) => {
-        if (prevTime === 0) {
-          // Switch to the other set of time
-          const isPomodoro = isPomodoroRef.current;
-          isPomodoroRef.current = !isPomodoro; // Toggle the session
-
-          // Return the time for the next session (Pomodoro or Break)
-          const newTimer = isPomodoro ? breakLength : pomodoroLength;
-          return newTimer; // Switch time between 10 and 15 seconds
+        if (prevTime <= 1) {
+          totalTimeRef.current -= isPomodoro ? pomodoroLength : breakLength;
+          if(isPomodoro){setWorkedTime((prev) => prev + pomodoroLength)}
+          setIsPomodoro((prevIsPomodoro) => !prevIsPomodoro);
+  
+          if (totalTimeRef.current <= 0) {
+            setTimerIsActive(false);
+            handleSuccesfullSession()
+          }
+          return isPomodoro ? breakLength : pomodoroLength;
         }
         return prevTime - 1;
       });
     }, 1000);
-
+  
     return () => clearInterval(interval);
-  }, [timerIsActive, pomodoroLength, breakLength]);
+  }, [timerIsActive, pomodoroLength, breakLength, isPomodoro]);
+  
 
-    const formatTime = (seconds) => {
-      const hours = Math.floor(seconds / 3600).toString().padStart(2, '0');
-      const minutes = Math.floor((seconds % 3600) / 60).toString().padStart(2, '0');
-      const secs = (seconds % 60).toString().padStart(2, '0');
-      return `${hours}:${minutes}:${secs}`;
-    };
+  const formatTime = (seconds) => {
+    const hours = Math.floor(seconds / 3600)
+      .toString()
+      .padStart(2, "0");
+    const minutes = Math.floor((seconds % 3600) / 60)
+      .toString()
+      .padStart(2, "0");
+    const secs = (seconds % 60).toString().padStart(2, "0");
+    return `${hours}:${minutes}:${secs}`;
+  };
 
   const handleNewSession = (e) => {
     e.preventDefault();
     const formData = new FormData(e.target);
     const payload = Object.fromEntries(formData);
     const totalTime = payload.time + ":00";
+    setSessionType(payload.type)
 
     const [totalhours, totalminutes, totalseconds] = totalTime
       .split(":")
-      .map(Number);      
-      setTotalTimeSeconds(totalhours * 3600 + totalminutes * 60 + totalseconds);
+      .map(Number);
 
-    
-      setTimerIsActive(true);
+    const totalTimeSeconds =
+      totalhours * 3600 + totalminutes * 60 + totalseconds;
+    totalTimeRef.current = totalTimeSeconds;
 
-    
-    
-    //onClickReset(preferedTimeSeconds);
+    const [preferedhours, preferedminutes, preferedseconds] = preferedTime
+      .split(":")
+      .map(Number);
+    const [preferedBreakhours, preferedBreakminutes, preferedBreakseconds] =
+      preferedBreak.split(":").map(Number);
+
+    const preferedTimeSeconds =
+      preferedhours * 3600 + preferedminutes * 60 + preferedseconds;
+
+    const preferedBreakSeconds =
+      preferedBreakhours * 3600 +
+      preferedBreakminutes * 60 +
+      preferedBreakseconds;
+
+    const cycle_length = preferedBreakSeconds + preferedTimeSeconds;
+
+    const amount_of_cycles = Math.floor(totalTimeSeconds / cycle_length);
+
+    const rest_time = totalTimeSeconds - amount_of_cycles * cycle_length;
+
+    const time_per_setting = rest_time / amount_of_cycles;
+
+    if (algoritmSetting === "longerBreak") {
+      setPomodoroLength(preferedTimeSeconds);
+      setBreakLength(preferedBreakSeconds + time_per_setting);
+    } else {
+      setPomodoroLength(preferedTimeSeconds + time_per_setting);
+      setBreakLength(preferedBreakSeconds);
+    }
+    setTimer(preferedTimeSeconds);
+    setTimerIsActive(true);
   };
+
+  const handleSuccesfullSession = () => {
+    let date = new Date(Date.now()).toISOString()
+    const dates = date.split("T")
+    date = dates[0]
+    
+    const session = {
+      type: sessionType,
+      totalTime: formatTime(workedTime),
+      dateCreated: date
+    }
+    console.log(session);
+    
+  }
 
   return (
     <div className="bg-tomato-50 grid grid-cols-4 h-screen">
@@ -181,7 +120,11 @@ export default function Pomodoro() {
       </div>
       <div className="col-span-3 bg-blue-500 flex flex-col items-center">
         {timerIsActive ? (
-          <h2>{formatTime(timer)}</h2>
+          <div>
+            <h2>{formatTime(workedTime)}</h2>
+            <h2>{formatTime(totalTimeRef.current)}</h2>
+            <h2>{formatTime(timer)}</h2>
+          </div>
         ) : (
           <div>
             <h2>Create a new Session</h2>
