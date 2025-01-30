78d518d9cc7e1e8829623e1e3d58fea71ec4e389
boel78 <95147718+boel78@users.noreply.github.com>
2025-01-30 10:38:11 -0700

    responsiv cancelconfirm

--- PomodoroFrontend/pomodoro/components/ConfirmCancel.tsx
+++ PomodoroFrontend/pomodoro/components/ConfirmCancel.tsx
@@ -11,11 +11,11 @@ export default function ConfirmCancel({setShowCancelWindow, handleSuccesfullSess
         handleSuccesfullSession()
     }
     return(
-        <div className="absolute z-20 left-0 px-4 bg-white h-1/4 flex flex-col items-center gap-12 shadow-md pt-4">
-                <h2 className="font-semibold text-tomato-600 text-md text-nowrap">Are you sure you want to cancel?</h2>
+        <div className="absolute z-20 left-0 md:left-1/2 px-4 bg-white h-1/4 md:h-1/3 flex flex-col items-center gap-12 md:gap-20 shadow-md pt-4">
+                <h2 className="font-semibold text-tomato-600 text-md md:text-xl text-nowrap">Are you sure you want to cancel?</h2>
                 <div className="grid grid-cols-2">
-                    <Button variant={'outline'} className="bg-tomato-700 text-tomato-50 text-xs" onClick={() => setShowCancelWindow(false)}>Go back</Button>
-                    <Button variant={'outline'} className="bg-green-600 text-tomato-50 text-xs" onClick={handleCancel}>Cancel the session</Button>
+                    <Button variant={'outline'} className="bg-tomato-700 text-tomato-50 text-xs md:text-lg" onClick={() => setShowCancelWindow(false)}>Go back</Button>
+                    <Button variant={'outline'} className="bg-green-600 text-tomato-50 text-xs md:text-lg" onClick={handleCancel}>Cancel the session</Button>
                 </div>
         </div>
     )
